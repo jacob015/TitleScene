570d4ae29840d099fb48099f6aa51a8a460132e3
jacob015 <jacob015@naver.com>
2025-02-07 20:35:17 +0900

    스와이프 오류 해결중

--- Assets/LobbyMain/Main/Scripts/SlideManager.cs
+++ Assets/LobbyMain/Main/Scripts/SlideManager.cs
@@ -1,63 +1,53 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using DG.Tweening;
 using UnityEngine.EventSystems;
+using DG.Tweening;
 
-public class SlideManager : MonoBehaviour, IDragHandler, IEndDragHandler
+public class SlideManager : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
 {
-    public RectTransform slidePanel;
-    public int totalPages = 3; // 총 페이지 수
-    private int currentPage = 0;
-    private float screenWidth;
-    private Vector2 startPosition;
+    public RectTransform panelContainer;
+    public RectTransform[] pages; 
+    private int currentPage = 1;
+    private float slideDuration = 0.5f; 
+    private Vector2 dragStartPos;
 
     void Start()
     {
-        screenWidth = Screen.width;
-        UpdateSlidePosition();
+        UpdatePanelPosition();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        dragStartPos = eventData.position;
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        float delta = eventData.delta.x;
-        slidePanel.anchoredPosition += new Vector2(delta, 0);
-        Debug.Log("Drag");
+        // 실시간으로 따라 움직이게 하고 싶다면 여기서 panelContainer.anchoredPosition 업데이트 가능
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        float dragThreshold = screenWidth * 0.2f; // 일정 거리 이상 이동해야 페이지 변경
-        if (eventData.pressPosition.x - eventData.position.x > dragThreshold)
+        float dragDelta = eventData.position.x - dragStartPos.x;
+        float threshold = Screen.width * 0.2f; // 화면 너비의 20%를 이동 기준으로 설정
+
+        if (Mathf.Abs(dragDelta) > threshold) 
         {
-            NextPage();
+            if (dragDelta < 0 && currentPage < pages.Length - 1) 
+            {
+                currentPage++;
+            }
+            else if (dragDelta > 0 && currentPage > 0) 
+            {
+                currentPage--;
+            }
         }
-        else if (eventData.position.x - eventData.pressPosition.x > dragThreshold)
-        {
-            PreviousPage();
-        }
-        else
-        {
-            UpdateSlidePosition();
-        }
+
+        UpdatePanelPosition();
     }
 
-    private void NextPage()
+    void UpdatePanelPosition()
     {
-        currentPage = (currentPage + 1) % totalPages; // 마지막 페이지에서 첫 페이지로 루프
-        UpdateSlidePosition();
-    }
-
-    private void PreviousPage()
-    {
-        currentPage = (currentPage - 1 + totalPages) % totalPages; // 첫 페이지에서 마지막 페이지로 루프
-        UpdateSlidePosition();
-    }
-
-    private void UpdateSlidePosition()
-    {
-        float targetX = -currentPage * screenWidth;
-        slidePanel.DOAnchorPos(new Vector2(targetX, 0), 0.5f).SetEase(Ease.OutQuad);
-        Debug.Log("Slide to page " + currentPage);
+        Vector2 targetPos = new Vector2(-pages[currentPage].anchoredPosition.x, 0);
+        panelContainer.DOAnchorPos(targetPos, slideDuration).SetEase(Ease.OutCubic);
     }
 }
