96305d41970d9b19e01240e5255cd770e6164362
jacob015 <jacob015@naver.com>
2025-02-07 21:03:01 +0900

    첫번째 빌드

--- Assets/LobbyMain/Main/Scripts/SlideManager.cs
+++ Assets/LobbyMain/Main/Scripts/SlideManager.cs
@@ -1,12 +1,13 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 using DG.Tweening;
+using UnityEngine.UI;
 
-public class SlideManager : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+public class SlideManager : MonoBehaviour
 {
-    public RectTransform panelContainer;
+    /*public RectTransform panelContainer; 
     public RectTransform[] pages; 
-    private int currentPage = 1;
+    private int currentPage = 0;
     private float slideDuration = 0.5f; 
     private Vector2 dragStartPos;
 
@@ -22,13 +23,13 @@ public class SlideManager : MonoBehaviour, IDragHandler, IBeginDragHandler, IEnd
 
     public void OnDrag(PointerEventData eventData)
     {
-        // 실시간으로 따라 움직이게 하고 싶다면 여기서 panelContainer.anchoredPosition 업데이트 가능
+        
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        float dragDelta = eventData.position.x - dragStartPos.x;
-        float threshold = Screen.width * 0.2f; // 화면 너비의 20%를 이동 기준으로 설정
+        float dragDelta = eventData.position.x - dragStartPos.x; 
+        float threshold = Screen.width * 0.2f; 
 
         if (Mathf.Abs(dragDelta) > threshold) 
         {
@@ -48,6 +49,54 @@ public class SlideManager : MonoBehaviour, IDragHandler, IBeginDragHandler, IEnd
     void UpdatePanelPosition()
     {
         Vector2 targetPos = new Vector2(-pages[currentPage].anchoredPosition.x, 0);
+        panelContainer.DOAnchorPos(targetPos, slideDuration).SetEase(Ease.OutCubic);
+    }*/
+    
+    public RectTransform panelContainer;
+    public RectTransform[] pages; 
+    public Button nextButton; 
+    public Button prevButton; 
+
+    private int currentPage = 0;
+    private float slideDuration = 0.3f; 
+    private float pageWidth;
+
+    void Start()
+    {
+        if (pages.Length == 0) return;
+
+        pageWidth = pages[0].rect.width; 
+        panelContainer.anchoredPosition = new Vector2(0, panelContainer.anchoredPosition.y); 
+
+        nextButton.onClick.AddListener(MoveNext);
+        prevButton.onClick.AddListener(MovePrevious);
+        Debug.Log("Page Width: " + pageWidth);
+        Debug.Log("Panel Initial Position: " + panelContainer.anchoredPosition);
+    }
+
+    public void MoveNext()
+    {
+        if (currentPage < 1)
+        {
+            currentPage++;
+            UpdatePanelPosition();
+        }
+    }
+
+    public void MovePrevious()
+    {
+        if (currentPage > -1)
+        {
+            currentPage--;
+            UpdatePanelPosition();
+        }
+    }
+
+    void UpdatePanelPosition()
+    {
+        Debug.Log("Moving to page: " + currentPage);
+        float targetX = -currentPage * pageWidth; // 페이지 너비만큼 이동
+        Vector2 targetPos = new Vector2(targetX, panelContainer.anchoredPosition.y);
         panelContainer.DOAnchorPos(targetPos, slideDuration).SetEase(Ease.OutCubic);
     }
 }
